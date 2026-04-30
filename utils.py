33be7f6730561761129079224dbc56d4c8df9657
Ammar Khan <ammark149@gmail.com>
2026-04-30 14:49:08 +0100

    Dictionary structure to be called upon and JSON file data persistence

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ def load_data(file_location):
     with open(file_location, 'r') as f:
         data = json.load(f)
     return data
+
+#Saving data for order management
+def save_data(file_location, data):
+    with open(file_location, 'w') as f:
+        json.dump(data, f, indent=4)
